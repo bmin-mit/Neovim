0c6bf1ddbb09f704ec4a517644d394cc2c59d75d
Doan Ngoc Binh Minh <bmmit.work@gmail.com>
2026-03-23 00:29:10 +0700

    colorscheme: themery (theme switcher), onedark, catppuccin, tokyonight

---
+++ lua/plugins/themes/themery.lua
@@ -0,0 +1,83 @@
+-- A colorscheme picker with live preview and persistence for neovim
+
+return {
+	"zaldih/themery.nvim",
+	lazy = false,
+	keys = {
+		{ "<C-k><C-T>", "<cmd>Themery<cr>", desc = "Change [T]heme" },
+	},
+	config = function()
+		require("themery").setup({
+			themes = {
+				{
+					name = "Catppucin",
+					colorscheme = "catppuccin-nvim",
+				},
+				{
+					name = "Catppucin Latte",
+					colorscheme = "catppuccin-latte",
+				},
+				{
+					name = "Catppucin Frappe",
+					colorscheme = "catppuccin-frappe",
+				},
+				{
+					name = "Catppucin Macchiato",
+					colorscheme = "catppuccin-macchiato",
+				},
+				{
+					name = "Catppucin Mocha",
+					colorscheme = "catppuccin-mocha",
+				},
+				{
+					name = "Tokyo Night",
+					colorscheme = "tokyonight",
+				},
+				{
+					name = "Tokyo Night - Day",
+					colorscheme = "tokyonight-day",
+				},
+				{
+					name = "Tokyo Night - Night",
+					colorscheme = "tokyonight-night",
+				},
+				{
+					name = "Tokyo Night - Storm",
+					colorscheme = "tokyonight-storm",
+				},
+				{
+					name = "Tokyo Night - Moon",
+					colorscheme = "tokyonight-moon",
+				},
+				{
+					name = "OneDark - Dark",
+					colorscheme = "onedark",
+					before = [[
+            require("onedark").setup({ style = "dark" })
+          ]],
+				},
+				{
+					name = "OneDark - Warmer",
+					colorscheme = "onedark",
+					before = [[
+            require("onedark").setup({ style = "warmer" })
+          ]],
+				},
+				{
+					name = "OneDark - Cool",
+					colorscheme = "onedark",
+					before = [[
+            require("onedark").setup({ style = "cool" })
+          ]],
+				},
+				{
+					name = "OneDark - Deep",
+					colorscheme = "onedark",
+					before = [[
+            require("onedark").setup({ style = "deep" })
+          ]],
+				},
+			},
+		})
+	end,
+}
