920282e076d5b94fc4eef2dec0e49c5c25dab5b1
Doan Ngoc Binh Minh <bmmit.work@gmail.com>
2026-03-24 22:44:37 +0700

    add treesitter configs

--- init.lua
+++ init.lua
@@ -29,6 +29,7 @@ require("lazy").setup({
 		{ import = "plugins.formatting" },
 		{ import = "plugins.themes" },
 		{ import = "plugins.utils" },
+		{ import = "plugins.treesitter" },
 	},
 	-- Configure any other settings here. See the documentation for more details.
 	-- colorscheme that will be used when installing plugins.
