cbaa6466c38e1201990ea38e0bd4220c77f43d17
Doan Ngoc Binh Minh <bmmit.work@gmail.com>
2026-03-24 23:17:44 +0700

    add bufferline

---
+++ lua/plugins/ui/bufferline.lua
@@ -0,0 +1,28 @@
+return {
+	"akinsho/bufferline.nvim",
+	version = "*",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	event = "VeryLazy",
+	keys = {
+		{ "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle Pin" },
+		{ "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete Non-Pinned Buffers" },
+		{ "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete Buffers to the Right" },
+		{ "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete Buffers to the Left" },
+	},
+	opts = {
+		options = {
+			diagnostics = "nvim_lsp",
+			offsets = {
+				{
+					filetype = "neo-tree",
+					text = "Neo-tree",
+					highlight = "Directory",
+					text_align = "left",
+				},
+				{
+					filetype = "snacks_layout_box",
+				},
+			},
+		},
+	},
+}
